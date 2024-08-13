62cd35097a722cf46e5fb90ba4c43479a334e0b7
Vijay Kumar <saroyav3@gmail.com>
2024-08-14 11:11:26 +1200

    created new files

---
+++ food_delivery_app/src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+
+export defualt function Home(){
+    return(
+        <div>
+
+            <div></Navbar></div>
+        
+        <div/>
+    )
+}
+
+//export default App;
