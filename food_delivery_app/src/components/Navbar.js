f28f1842a3a8d4711bf841ea8fb4d597d696abef
Vijay Kumar <saroyav3@gmail.com>
2024-08-15 17:36:50 +1200

    working on implementing card

--- food_delivery_app/src/components/Navbar.js
+++ food_delivery_app/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">BiteBuddy</Link>
+    <Link className="navbar-brand fs-1" to="/">BiteBuddy</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
