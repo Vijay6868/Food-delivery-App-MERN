edf080485bbc0156b304b957c3ce21de273b28d2
Vijay Kumar <saroyav3@gmail.com>
2024-08-15 19:38:48 +1200

    created card component

--- food_delivery_app/src/screens/Home.js
+++ food_delivery_app/src/screens/Home.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
 
 export default function Home() {
     return (
         <div>
             <div><Navbar /></div>
-            <div><div className="card mt-3" style={{"width": "18rem"}}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-                </div>
-            </div></div>
+            <div>
+                <Card></Card>
+            </div>
             <div><Footer /></div>
         </div>
     )
